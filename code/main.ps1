46448294d6c1480106b8bb9784d41da9adb90ee8
Leime <tonywhang@naver.com>
2024-10-09 22:24:48 +0900

    make main

--- code/main.ps1
+++ code/main.ps1
@@ -1,9 +1,6 @@
 Clear-Host
 
 #region Foreground color functions
-    function greeting {
-        Write-Host "USK 정보 데이터베이스에 오신 것을 환영합니다."
-    }
 
     function Green {
         process { Write-Host $_ -ForegroundColor Green }
@@ -27,15 +24,48 @@ Clear-Host
 
 #endregion
 
-#region display functions
+#region core functions
+
+    function mainMenu {
+        Write-Output "미구현" | Cyan
+    }
+
+    function login {
+        $attempt = 0
+        $login = $false
+        do {
+            $username = Read-Host "사용자 이름을 입력하십시오"
+            $password = Read-Host -AsSecureString "비밀번호를 입력하십시오"
+            $pwd_text = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
+            if ($username -eq "annonymus" -and $pwd_text -ceq "mirbyungsinanarchythebest") {
+                $login = $true
+                Write-Output "로그인에 성공하였습니다." | Green
+                mainMenu
+            } else {
+                if ($attempt -eq 2) {
+                    Write-Output "로그인 시도 횟수(3)를 초과하였습니다. 프로그램이 종료됩니다." | Red
+                    exit
+                }
+                Write-Output "로그인에 실패하였습니다. 다시 시도하십시오." | Red
+                $attempt++
+            }
+        } while (-not $login)
+    }
     function title {
-        Write-Host "====================================" | Cyan
-        Write-Host "        * USK 정보 데이터베이스       " | Red
-        Write-Host "====================================" | Cyan
+        Write-Output "====================================" | Cyan
+        Write-Output "        * USK 정보 데이터베이스       " | Red
+        Write-Output "====================================" | Cyan
     }
 
     function greeting {
-        Write-Host "USK 정보 데이터베이스에 오신 것을 환영합니다." | Green
+        Write-Output "USK 정보 데이터베이스에 오신 것을 환영합니다." | Green
+        Write-Output "접속을 진행하려면 아무 키나 누르십시오. 종료하려면 콘솔을 끄십시오. " | Yellow
+        Write-Output "주의: 허가되지 않은 인원의 접근은 Berryman-Langford 밈적 살해인자로 인해 즉시 미르화로 이어질 수 있습니다." | Red
+        Write-Output "접속을 진행하려면 아무 키나 누르십시오..." | Yellow
+        $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+        Write-Output "로그인을 진행합니다." | Green
+        login
+        
     }
 #endregion
 
@@ -43,6 +73,3 @@ Clear-Host
     title
     greeting
 #endregion
-
-Write-Output "this is a test" | Green
-Write-Output "this is a test" | Red
